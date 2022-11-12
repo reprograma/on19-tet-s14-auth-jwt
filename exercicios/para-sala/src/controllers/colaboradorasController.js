dcd4558685909a3f6b5b73a619dba7b3d90d7adb
Lydianne Corrêa <lydianne.correa2@gmail.com>
2022-11-12 16:45:53 -0300

    get da tarde

--- exercicios/para-sala/src/controllers/colaboradorasController.js
+++ exercicios/para-sala/src/controllers/colaboradorasController.js
@@ -1,8 +1,26 @@
 const colaboradoras = require("../models/colaboradoras");
+const SECRET = process.env.SECRET;
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
 const getAll = (req, res) => {
+  const authHeader = req.get(`authorization`);
+  const token = authHeader.split("")[1];
+  console.log(`Meu header:`, token);
+
+  if (!token) {
+    return res.status(401);
+  }
+
+  jwt.verify(token, SECRET, function (Erro) {
+    if (Erro) {
+      return res.status(401).send("Não autorizado");
+    }
+  });
+
   console.log(req.url);
   colaboradoras.find(function (err, colaboradoras) {
-    res.status(200).send(colaboradoras);
+    res.status(200);
   });
 };
 
@@ -18,6 +36,6 @@ const postColaboradora = (req, res) => {
 };
 
 module.exports = {
-    getAll,
-    postColaboradora
-}
+  getAll,
+  postColaboradora,
+};
