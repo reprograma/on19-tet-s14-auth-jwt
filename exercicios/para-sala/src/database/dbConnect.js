a813860f928ec8a4ca4d23e9ef7d4dbde375fd55
Djeisly Rodrigues <djeisly@yahoo.com.br>
2022-11-16 22:55:16 -0300

    update

--- exercicios/para-sala/src/database/dbConnect.js
+++ exercicios/para-sala/src/database/dbConnect.js
@@ -1,4 +1,27 @@
+const mongoose = require('mongoose')
 const DATABASE_MONGO = process.env.DATABASE_MONGO
+
+const db = async () => {
+    try {
+        await mongoose.connect(DATABASE_MONGO,{
+            useNewUrlParser: true,
+            
+            useUnifiedTopology: true
+        })
+        console.log("Banco conectado :)")
+    } catch (error) {
+        console.log("Erro: ", error.message)
+    }
+}
+
+module.exports = {
+    db
+}
+
+
+
+
+/*const DATABASE_MONGO = process.env.DATABASE_MONGO
 const mongoose = require ("mongoose");
 
 mongoose.connect (DATABASE_MONGO, {
@@ -12,4 +35,23 @@ mongoose.connect(
 
 const db = mongoose.connection;
 
-module.exports = db;
+module.exports = db;*/
+
+
+
+
+
+
+
+
+/*const mongoose = require('mongoose');
+
+const password = 'UzMYVD7HVeKFgLLE';
+
+mongoose.connect(
+  `mongodb+srv://admin:${password}@cluster0.1vuycki.mongodb.net/reprograma` 
+);
+
+const db = mongoose.connection;
+
+module.exports = db;*/
