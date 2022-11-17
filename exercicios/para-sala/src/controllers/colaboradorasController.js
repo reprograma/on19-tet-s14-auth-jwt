7774908e21c9497d291606b41a18c343f6ce21c8
pâmela santos <pssantos201@gmail.com>
2022-11-16 21:03:23 -0300

    :rocket: feat: add controller

--- exercicios/para-sala/src/controllers/colaboradorasController.js
+++ exercicios/para-sala/src/controllers/colaboradorasController.js
@@ -1,23 +1,23 @@
-const colaboradoras = require("../models/colaboradoras");
+const colaboradoras = require('../models/colaboradoras');
 const SECRET = process.env.SECRET
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 
 const getAll = (req, res) => {
-  const authHeader = req.get(`authorization`);
+  const authHeader = req.get('authorization');
   const token = authHeader.split(' ')[1];
   console.log(`Meu header:`, token);
 
-  if(!token){
-    return res.status(401)
+  if (!token) {
+    return res.status(401).send('Header error.');
   }
 
-  const err = jwt.verify(token,SECRET,function(error){
-    if(error) return error 
+  const err = jwt.verify(token, SECRET, function (error) {
+    if (error) return error
   })
-  
-  if (err) return res.status(401).send("não autorizado")
+
+  if (err) return res.status(401).send('Not authorized.')
 
   console.log(req.url);
   colaboradoras.find(function (err, colaboradoras) {
@@ -25,32 +25,31 @@ const getAll = (req, res) => {
   });
 };
 
-
 const postColaboradora = (req, res) => {
-  const senhaComHash = bcrypt.hashSync(req.body.password, 10);
+  const senhaComHash = bcrypt.hashSync(req.body.password,10)
   req.body.password = senhaComHash;
 
   console.log(req.body);
-  const colaboradora = new colaboradoras(req.body);
 
+  let colaboradora = new colaboradoras(req.body);
+  
   colaboradora.save(function (err) {
-    if (err) {
-      return res.status(500)
-    }
+    if (err) res.status(500).send({ message: err.message });
+
     res.status(201).send(colaboradora.toJSON());
   });
 };
 
 const login = (req,res) => {
   colaboradoras.findOne({ email: req.body.email }, function(error, colaboradora) {
-    if(!colaboradoras) {
-      return res.status(404).send(`Não localizamos o email ${req.body.email}`);
+    if(!colaboradora) {
+      return res.status(404).send(`Email not found ${req.body.email}`);
     }
 
-    const senhaValida = bcrypt.compareSync(req.body.password, colaboradoras.password);
+    const senhaValida = bcrypt.compareSync(req.body.password, colaboradora.password);
 
     if(!senhaValida) {
-      return res.status(403).send(`Esta senha está incorreta`)
+      return res.status(403).send('Incorrect password')
     }
 
     const token = jwt.sign({ email: req.body.email }, SECRET);
