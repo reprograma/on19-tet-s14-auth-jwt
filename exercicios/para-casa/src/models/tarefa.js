c1292cbd0d971606dc88249022cae8ccb0402e95
Aline Hoffmann <alinechoff@gmail.com>
2022-11-17 16:16:32 -0300

    Tarefa Semana 14

---
+++ exercicios/para-casa/src/models/tarefa.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const tarefaSchema = new mongoose.Schema({
+    id : { type : Number},
+    descricao: { type: String },
+    dataInclusao: { type: String },
+    concluido: { type: Boolean },
+    nome: { type: String },
+    password: { type: String },
+
+},{
+    versionKey: false
+})
+
+const tarefas = mongoose.model('tarefa', tarefaSchema);
+
+module.exports = tarefas
