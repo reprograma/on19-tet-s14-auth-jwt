d0ef04a35246cdc341f6f02bf4552ea5f09886bb
Mariana Cristina Dias dos Santos <marianacristinasantos05@gmail.com>
2022-11-22 17:32:25 -0300

    add tarefas Route file

---
+++ exercicios/para-sala/src/routes/tarefasRoute.js
@@ -0,0 +1,9 @@
+const controller = require("../controllers/tarefasController");
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/tarefas", controller.getAll);
+
+
+module.exports = router;
